9ffb8eb2dec11712e364c3ae3fb344a486beeb12
berkinory <berk@mirac.dev>
2025-11-13 14:49:22 +0300

    Update Sidebar component styles for improved layout consistency
    - Adjusted width calculations for collapsible icons in the Sidebar component to enhance visual alignment.
    - Modified spacing values to ensure a more cohesive design across different sidebar variants.

--- web/src/components/ui/sidebar.tsx
+++ web/src/components/ui/sidebar.tsx
@@ -262,7 +262,7 @@ function Sidebar({
           'group-data-[collapsible=offcanvas]:w-0',
           'group-data-[side=right]:rotate-180',
           variant === 'floating' || variant === 'inset'
-            ? 'group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4)))]'
+            ? 'group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(2)))]'
             : 'group-data-[collapsible=icon]:w-(--sidebar-width-icon)'
         )}
         data-slot="sidebar-gap"
@@ -274,7 +274,7 @@ function Sidebar({
             ? 'left-0 group-data-[collapsible=offcanvas]:left-[calc(var(--sidebar-width)*-1)]'
             : 'right-0 group-data-[collapsible=offcanvas]:right-[calc(var(--sidebar-width)*-1)]',
           variant === 'floating' || variant === 'inset'
-            ? 'p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(4))+2px)]'
+            ? 'p-2 group-data-[collapsible=icon]:w-[calc(var(--sidebar-width-icon)+(--spacing(2))+2px)]'
             : 'group-data-[collapsible=icon]:w-(--sidebar-width-icon) group-data-[side=left]:border-r group-data-[side=right]:border-l',
           className
         )}
